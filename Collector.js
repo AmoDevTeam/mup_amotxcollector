9aff1b765b3e47dfe07c054c401714cf1e0a77bd
Sangoh Lee <andylee2015123@gmail.com>
2023-12-19 14:11:59 +0900

    Collector.js 수정

--- Collector.js
+++ Collector.js
@@ -1,8 +1,10 @@
 const KasApi = require('./KasApi.js');
 
+// 현재 시점부터 전부 이전의 기록 전부 다 가져올 때 사용함.
+// 평소에는 딱히 사용할 필요가 없으며, 최초에 초기화할 때만 사용하면 됨.
 async function collectorMain() {
     let kasApi = new KasApi();
-    while (1) {
+    while (!KasApi.checkEnded()) {
         // 요청 api 실행 - 이전 커서 사용
         await kasApi.getTxList(true); 
 
